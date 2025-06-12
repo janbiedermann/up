837a2a922f2357dcc38382156d4846e1333b830f
Jan Biedermann <jan@isomorfeus.com>
2025-06-12 23:27:19 +0200

    Update compile options

--- ext/up_ext/extconf.rb
+++ ext/up_ext/extconf.rb
@@ -1,5 +1,6 @@
 require 'mkmf'
 
-$CFLAGS << ' -O3 -Wall -DLIBUS_NO_SSL '
+$CFLAGS << ' -O3 -Wall -DLIBUS_NO_SSL -Wc++17-extensions '
+$CPPFLAGS << ' -O3 -Wall -DLIBUS_NO_SSL -Wc++17-extensions '
 
 create_makefile('up_ext')
